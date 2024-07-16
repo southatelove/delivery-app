5fe0fc4d92228e864fb0cb294fa96de688e01fa5
southatelove <artem.jlez@gmail.com>
2024-07-16 10:43:29 +0300

    update: useLocation тест

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,8 +1,16 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useLocation } from "react-router-dom";
 import styles from "./Layout.module.css";
 import { Button } from "../components/Button/Button";
+import { useEffect } from "react";
+
+import cn from "classnames";
 
 export default function Layout() {
+  const location = useLocation();
+
+  useEffect(() => {
+    console.log("location", location);
+  }, [location]);
   return (
     <>
       <div className={styles["layout"]}>
@@ -13,11 +21,16 @@ export default function Layout() {
             <div className={styles["email"]}>artem.jlez@gmail.com</div>
           </div>
           <div className={styles["menu"]}>
-            <Link to="/" className={styles["link"]}>
+            <Link
+              to="/"
+              className={cn(styles["link"], {
+                [styles.active]: location.pathname === "/",
+              })}
+            >
               <img src="/menu.svg" alt="menu" />
               Меню
             </Link>
-            <Link to="/cart.svg" className={styles["link"]}>
+            <Link to="/cart" className={styles["link"]}>
               <img src="/cart.svg" alt="cart" />
               Корзина
             </Link>
